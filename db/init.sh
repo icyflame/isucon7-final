eaedb7cd5b9677e820a9b134ca5df0f066f3c412
Siddharth Kannan <mail@siddharthkannan.in>
2021-07-31 22:55:53 +0900

    Put root password in the ansible playbook

--- db/init.sh
+++ db/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $(dirname $0)
-mysql --default-character-set=utf8mb4 -uroot < $(pwd)/drop.sql
-mysql --default-character-set=utf8mb4 -uroot isudb < $(pwd)/isudb.sql
-mysql --default-character-set=utf8mb4 -uroot isudb < $(pwd)/m_item.sql
+mysql --default-character-set=utf8mb4 -uroot -pisucon < $(pwd)/drop.sql
+mysql --default-character-set=utf8mb4 -uroot isudb -pisucon < $(pwd)/isudb.sql
+mysql --default-character-set=utf8mb4 -uroot isudb -pisucon < $(pwd)/m_item.sql
